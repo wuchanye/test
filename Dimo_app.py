50f20c40be36ef9c79d3904faefa0a253759ef90
wuchanye <147586984+wuchanye@users.noreply.github.com>
2023-10-18 14:56:42 +0800

    Update Dimo_app.py

--- Dimo_app.py
+++ Dimo_app.py
@@ -25,7 +25,7 @@ import tempfile
 from datetime import datetime, timedelta
 
 line_bot_api = LineBotApi(os.environ.get('line_channel_token'))
-handler = WebhookHandler('os.environ.get('line_secret')')
+handler = WebhookHandler(os.environ.get('line_secret'))
 
 db_using=db
 
