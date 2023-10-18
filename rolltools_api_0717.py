bd7e2b9e9338830d7ac6efadccf886ea852bf565
wuchanye <147586984+wuchanye@users.noreply.github.com>
2023-10-18 13:07:09 +0800

    Update rolltools_api_0717.py

--- rolltools_api_0717.py
+++ rolltools_api_0717.py
@@ -1,21 +1,13 @@
-from opencc import OpenCC
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import StaleElementReferenceException
-from io import BytesIO
-from PIL import Image
-import pyimgur
 import urllib.request as request
 import urllib.parse as parse
-
 import os
 import requests
 import re
 import base64
 import json
 import time
+from bs4 import BeautifulSoup
+import hashlib
 
 # app_id=uxnxyhgkujxpgqbt&app_secret=UHJqRjR6S25DaSttRWxZUElSbnovUT09
 
@@ -104,7 +96,6 @@ def search(appid,secret,name):
     return c_list
 
 
-
 #利用foodId, 獲取食物詳細資訊, 經選擇的那筆資料為food_data 
 def query(appid,secret,food_id,unit,picON):
     
@@ -122,7 +113,7 @@ def query(appid,secret,food_id,unit,picON):
     # 擷取其中一筆資料的部分內容, 列印食物詳細資訊
     #name = data["data"]["name"]
     name = s2t.convert(data["data"]["name"])
-    img=download_images(1, name)
+    img_content=download_images_with_beautifulsoup(round, name)
     time.sleep(1) #訪問間隔
     calory = data["data"]["calory"]
     calory_unit = data["data"]["caloryUnit"]
@@ -134,159 +125,100 @@ def query(appid,secret,food_id,unit,picON):
     carbohydrate_unit = data["data"]["carbohydrateUnit"]
     print("名稱：",name,"\n熱量：",calory,calory_unit,"\n蛋白質：",protein,protein_unit,"\n脂質：",fat,fat_unit,"\n碳水化合物：",carbohydrate,carbohydrate_unit)
     print('IMAGE_PATH='+str(img))
-    imgUrl=upload_to_imgur(img)
+    image_url=upload_image_to_github(image_content, name, github_token)
     data={'name':name,'calory':calory,'protein':protein,'fat':fat,'carb':carbohydrate}
     dataAndunit={'name':name,'calory':calory+' '+ calory_unit,'protein':protein+' '+protein_unit,'fat':fat+' '+fat_unit,'carb':carbohydrate+' '+carbohydrate_unit}
     if unit==True and picON==True:
-        return dataAndunit,imgUrl
+        return dataAndunit,image_url
     elif unit==True and picON==False:
         return dataAndunit
     else:
         return data
     
-# 獲取Chrome驅動，訪問URL
-def init_browser(img_keyword):
+def download_images_with_beautifulsoup(round, img_keyword):
+    # 獲取 html 內容
     url = f'https://www.google.com.hk/search?q={img_keyword}&tbm=isch'
-    
-    chrome_options = webdriver.ChromeOptions()
-    
-    chrome_options.add_argument("--disable-infobars") # 禁用一些瀏覽器的信息欄
-    chrome_options.add_argument("--headless")
-    
-    # 創建一個帶有特定配置的 Chrome 瀏覽器實例
-    chromedriver_path = "D:\\LineBotWithPython\\DIMO\\DIMO3.0\\chromedriver-win64\\chromedriver.exe"
-    browser = webdriver.Chrome(executable_path=chromedriver_path,options=chrome_options)
-    
-    browser.get(url)
-    browser.maximize_window()
-    
-    return browser
+    resp = requests.get(url)
+    soup = BeautifulSoup(resp.text, 'lxml')
 
-
-# 下載圖片
-def download_images(round, img_keyword):
+    data_url_count = 0
+    max_image_size = 1024 * 1024
+    img_content = None  # 用于存储图像内容
     
-    existing_image_path = check_existing_images(img_keyword)
-    if existing_image_path:
-        print(f"使用現有圖片：{existing_image_path}")
-        return existing_image_path
-    else:
-        browser = init_browser(img_keyword)
-        local_folder = 'imgs4food'
-        
-        if not os.path.exists(local_folder): # 資料夾不存在時創建一個
-            os.makedirs(local_folder)
+    for i in range(round):
+        try:
+            # 分析網頁, 找圖片元素
+            img_elements = soup.find_all('img') 
             
-        # img_url_dic = [] # 記錄下載過的圖片地址，避免重複下載
-        # count = 0  # 圖片序號
-        data_url_count = 0  # 計數 Data URL 圖片數量
-        max_image_size = 1024 * 1024  # 目前圖片限制大小為 1 MB
+            for element in img_elements:
+                # 遍歷元素, 鎖定屬性
+                img_url = element.get('src')
+                if isinstance(img_url, str) and img_url.startswith('https://'): # 留下https://開頭的url
+                    if data_url_count == 0:
+                        data_url_count += 1
+                        continue
+                    else:
+                        r = requests.get(img_url)
+                        if len(r.content) < max_image_size: 
+                            img_content = r.content # 將二進制內容儲存在參數
+                            break
+        except Exception as e:
+            print(f"出现错误：{str(e)}")
+            continue
         
-        for i in range(round):
-            try:
-                # 分析網頁,找到圖片元素
-                parent_element = WebDriverWait(browser, 20).until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, '.islrc')))
-                
-                img_elements = WebDriverWait(parent_element, 20).until(
-                    EC.presence_of_all_elements_located((By.TAG_NAME, 'img')))
-                
-                # 遍歷元素 鎖定需要的屬性
-                for element in img_elements:
-                    img_url = element.get_attribute('src')
-                    
-                    img_attributes = element.get_attribute('outerHTML') # 獲取 img 標籤中的"所有"屬性
-                    
-                    # 確保'img_url'是一個字符串，排除 get_attribute('src') 返回 None 或其他非字符串值的問題。
-                    if isinstance(img_url, str):
-                        
-                        # 判斷是否是 Data URL
-                        if img_url.startswith('data:image/jpeg'): 
-                            if 'data-sz' not in img_attributes:  # 避免下載到網站小圖標
-                                if data_url_count == 0: # 跳過第一張
-                                    data_url_count += 1
-                                    continue  # 經過試驗 第二張好於第一張
-                                else:
-                                    img_data = re.search(r'base64,(.*)', img_url).group(1) # 提取 Data URL 中的圖片數據, 利用正則表達式捕獲base64,後面的全部
-                                    image_data = base64.b64decode(img_data)  # 解碼圖片數據成二進制數據
-                                    
-                                    if len(image_data) < max_image_size:
-                                        image = Image.open(BytesIO(image_data)) # 將圖片數據轉換為圖片對象
-                                        image = image.convert("RGB") # 將圖片轉換為 RGB 模式
-                                        
-                                        # 下載並保存圖片到當前目錄下
-                                        #filename = f'./imgs/{img_keyword}_{str(count)}.jpg'
-                                        filename = f'./imgs4food/{img_keyword}.jpg'
-                                        image.save(filename)
-                                        break
-                                        #count += 1
-                                        #print('this is ' + str(count) + 'st img')
-                                        # time.sleep(0.2) # 防止反爬機制
-                        else:
-                            
-                            if len(img_url) <= 200: # URL太長基本上就不是圖片的URL，先過濾掉，爬後面的
-                                
-                                # 判斷是否是普通的 PNG 圖檔
-                                if 'images' in img_url: 
-                                    
-                                    # 下載並保存圖片到當前目錄下
-                                    #filename = f'./imgs/{img_keyword}_{str(count)}.jpg'
-                                    filename = f'./imgs4food/{img_keyword}.jpg'
-                                    r = requests.get(img_url)
-                                    if len(r.content) < max_image_size:
-                                        with open(filename, 'wb') as file:
-                                            file.write(r.content)
-                                            file.close()
-                                            break
-                                            #count += 1
-                                            #print('this is ' + str(count) + 'st img')
-                                            # time.sleep(0.2) # 防止反爬機制
-                                
-            except StaleElementReferenceException:
-                print("出現StaleElementReferenceException錯誤，重新定位元素")
-                continue
-            
-        time.sleep(0.5)
-        browser.close()
-        print("爬取完成")
+    print("爬取完成")
+    time.sleep(0.5)
+    return img_content
+
+def upload_image_to_github(image_content, keyword, github_token):
+    github_username = 'wuchanye'
+    github_repo = 'test'
+    github_folder = 'imgs4food'
+    filename = keyword + '.jpg'
+    img_message = None
+    
+    
+    sha1 = hashlib.sha1()
+    sha1.update(image_content)
+    sha1_hash = sha1.hexdigest()
+    
+    url = f'https://api.github.com/repos/{github_username}/{github_repo}/contents/{github_folder}/{filename}'
+    headers = {
+        'Authorization': f'Bearer {github_token}',
+    }
+    
+    response = requests.get(url, headers=headers)
+    existing_file_data = response.json()
+    existing_sha = existing_file_data.get('sha', '')
+    
+    if existing_sha != sha1_hash:
         
-        return filename
-    
-def check_existing_images(img_keyword):
-    local_folder = 'imgs4food'
+        headers = {
+            'Authorization': f'Bearer {github_token}',
+            'Content-Type': 'application/json',
+        }
 
-    if not os.path.exists(local_folder):  # 資料夾不存在時，表示沒有相同關鍵字的圖片
-        return None
+        data = {
+            'message': 'Update image',
+            'content': base64.b64encode(image_content).decode('utf-8'),
+            'sha': existing_sha,
+        }
 
-    # 取得資料夾中所有的檔案名稱
-    existing_images = os.listdir(local_folder)
+        response = requests.put(url, headers=headers, json=data)
 
-    # 檢查是否有以 img_keyword 為名的圖片
-    for filename in existing_images:
-        if f'{img_keyword}.jpg' in filename:
-            return os.path.join(local_folder, filename)  # 返回已存在的圖片的完整路徑
-
-    # 如果沒有相同關鍵字的圖片，返回 None
-    return None
-
-# 上傳圖片 並取得url
-def upload_to_imgur(img_path):
-    
-    # Imgur API 密鑰
-    client_id = "1c126fdbf51fd36"
-    # client_secret = "e923e678d27d8f019913f26511a4d02d060899ba"
-    title = "Uploaded with PyImgur"
-    
-    im = pyimgur.Imgur(client_id)
-    # 上傳圖片並獲取圖片的 URL
-    uploaded_image = im.upload_image(img_path, title=title)
-    image_url = uploaded_image.link
-    
-    # img_message = ImageSendMessage(
-    #     original_content_url=f'{image_url}',  
-    #     preview_image_url=f'{image_url}'  
-    # )
+        if response.status_code == 200 or response.status_code == 201:
+            image_url = response.json().get('content').get('download_url')
+            #img_message = ImageSendMessage(
+            #    original_content_url=image_url,
+            #    preview_image_url=image_url
+            #)
+            print("文件已成功更新到GitHub存储库的文件夹。")
+        else:
+            print(f"上传文件失败，HTTP响应代码: {response.status_code}")
+            print(f"响应内容: {response.text}")
+            #img_message = TextSendMessage(text="上傳圖片失敗。")
     return image_url
+    
 
 if(__name__)=="__main__":
     keyword = input("請輸入食物名稱：")
